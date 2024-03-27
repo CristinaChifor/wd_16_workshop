a9c3a3155a057a730684b9447b24f9762ae7b6c5
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-27 19:00:04 +0200

    Add first example

--- script.js
+++ script.js
@@ -32,5 +32,21 @@ const categoryCssClasses = {
  */
 
 document.addEventListener('DOMContentLoaded', async () => {
-    // your code here
+    const listContainer = document.getElementById('products-container');
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then((products) => {
+        for (const product of products) {
+            const productCard = document.createElement('article');
+            productCard.classList.add('col-2-md', 'm-2', 'card', 'cursor-pointer');
+            productCard.innerHTML = `<img src="${product.image}" alt="Product Image">
+            <div class="card-content">
+                <div class="title">${product.title}</div>
+                <div class="price">$${product.price}</div>
+                <span class="badge rounded-pill ${categoryCssClasses[product.category]}">${product.category}</span>
+                <div class="description">${product.description}</div>
+            </div>`;
+            listContainer.appendChild(productCard);
+        }
+    });
 });
