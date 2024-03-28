2cf96740c0d8b1417b163acb606f53070287f6bc
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-28 21:37:34 +0200

    Fetch and display products

--- script.js
+++ script.js
@@ -30,33 +30,28 @@ const categoryCssClasses = {
     </article>
  * 
  */
+// obiectele sunt colectii de cheie: valoare
+console.log('works');
 
 document.addEventListener('DOMContentLoaded', async () => {
+
     const listContainer = document.getElementById('products-container');
-    const products = await getProducts();
-    
-        for (const product of products) {
-            const productCard = document.createElement('article');
-            productCard.classList.add('col-2-md', 'm-2', 'card', 'cursor-pointer');
-            productCard.innerHTML = `<img src="${product.image}" alt="Product Image">
-            <div class="card-content">
-                <div class="title">${product.title}</div>
-                <div class="price">$${product.price}</div>
-                <span class="badge rounded-pill ${categoryCssClasses[product.category]}">${product.category}</span>
-                <div class="description">${product.description}</div>
-            </div>`;
-            listContainer.appendChild(productCard);
-            productCard.addEventListener('click', () =>  {
-                localStorage.setItem('productId', product.id);
-                window.location.href = './product_page.html';
-            })
-        }});
 
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then((products) => {
+            for (const product of products) {
+                const productCard = document.createElement('article');
+                productCard.classList.add('col-2-md','m-2','card','cursor-pointer');
+                productCard.innerHTML = `<img src="${product.image}" alt="Product Image">
+                <div class="card-content">
+                    <div class="title">${product.title}</div>
+                    <div class="price">$${product.price}</div>
+                    <span class="badge rounded-pill ${categoryCssClasses[product.category]}">${product.category}</span>
+                    <div class="description">${product.description}</div>
+                </div>`;
+            listContainer.appendChild(productCard); }
 
-async function getProducts() {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const products = await response.json();
-    return products;
-}
-
+            });
+        });
 
