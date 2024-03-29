1e981600df32afe3bec8402d87e1cf0032451723
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-29 10:28:12 +0200

    Display products details

--- product.js
+++ product.js
@@ -8,9 +8,20 @@ document.addEventListener('DOMContentLoaded',
    console.log(productData);
    
    const imageContainer = document.getElementById('img-container');
-   imageContainer.innerHTML = `<img src=${productData.image}>`;
+   imageContainer.innerHTML = `<img class="img-fluid" src=${productData.image}>`;
   
    const title = document.getElementById('product-title');
    title.textContent = productData.title;
-   
+
+   const category = document.getElementById('product-categ');
+   category.textContent = productData.category;
+
+   const price = document.getElementById('product-price');
+   price.textContent = productData.price;
+
+   const description = document.getElementById('product-description');
+   price.textContent = productData.description;
+
+   const reviewCount = document.getElementById('review-count');
+   reviewCount.textContent = `of ${productData.rating.count} reviews`;
 });
